ee59ae6f8e86b22be082c6e94159489c67ccaa75
Alfred <alfredsklotsu@gmail.com>
2023-04-02 12:43:15 +0000

    looking sharp2

--- Components/LoginAuth.js
+++ Components/LoginAuth.js
@@ -1,20 +1,21 @@
 import {form_control, input, text, label, inputs, auth} from "../styles/Auth.module.css"
 import {motion} from "framer-motion";
+
 const LoginAuth = () => {
-    const attemptLogin = async() => {
+    const attemptLogin = async () => {
         let myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         let response = await fetch('https://go-jwt-auth-production.up.railway.app/users/login',
-        {
-            method: "POST",
-            body: JSON.stringify({
-                "email": "gaga12bobo @gmail.com",
-                "phone": "87383273232897",
-                "password": "3979s3239",
-            }),
-            headers: myHeaders,
-            credentials: 'include'
-        })
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    "email": "gaga12bobo @gmail.com",
+                    "phone": "87383273232897",
+                    "password": "3979s3239",
+                }),
+                headers: myHeaders,
+                credentials: 'include'
+            })
         await response.text()
         console.log(response)
         console.log(response.status)
@@ -22,31 +23,34 @@ const LoginAuth = () => {
     }
 
     return <form>
-              <div className={inputs}>
-                  <div className={form_control}>
-                      <div className={input}>
-                          <label className={label} htmlFor="email">Email</label>
-                          <motion.input className={text} type="text" name="email" id="email" placeholder={"Enter your email"} initial={{opacity: 0,y: "-40px"}} animate={{opacity: 1,y: 0}}/>
-                      </div>
-                  </div>
+        <div className={inputs}>
+            <div className={form_control}>
+                <div className={input}>
+                    <label className={label} htmlFor="email">Email</label>
+                    <motion.input className={text} type="text" name="email" id="email" placeholder={"Enter your email"}
+                                  initial={{opacity: 0, y: "-40px"}} animate={{opacity: 1, y: 0}}/>
+                </div>
+            </div>
 
 
-                  <div className={form_control}>
-                      <div className={input}>
-                          <label className={label} htmlFor="password">Password</label>
-                          <motion.input className={text} type="password" name="password" id="password" placeholder={"**** (8 characters minimum) **** "} initial={{opacity: 0, y: "-40px"}} animate={{opacity: 1, y: 0}}/>
-                      </div>
-                  </div>
+            <div className={form_control}>
+                <div className={input}>
+                    <label className={label} htmlFor="password">Password</label>
+                    <motion.input className={text} type="password" name="password" id="password"
+                                  placeholder={"**** (8 characters minimum) **** "} initial={{opacity: 0, y: "-40px"}}
+                                  animate={{opacity: 1, y: 0}}/>
+                </div>
+            </div>
 
-                  <div className={form_control}>
-                      <button className={auth} onClick={async (e)=> {
-                          e.preventDefault()
-                          await attemptLogin()
-                      }} type={"submit"}>Log In</button>
-                  </div>
-              </div>
-            </form>
-
+            <div className={form_control}>
+                <button className={auth} onClick={async (e) => {
+                    e.preventDefault()
+                    await attemptLogin()
+                }} type={"submit"}>Log In
+                </button>
+            </div>
+        </div>
+    </form>
 }
 
 export default LoginAuth
--- Components/SignUpAuth.js
+++ Components/SignUpAuth.js
@@ -3,29 +3,29 @@ import {motion} from "framer-motion";
 
 const attemptLogin = async () => {
     let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Content-Type", "application/json");
 
-        let response = await fetch('https://go-jwt-auth-production.up.railway.app/users/signup', {
-            method: 'POST',
-            body: JSON.stringify({
-                "fullName": "Henry Lotsu",
-                "email": "gaga12bobo @gmail.com",
-                "phone": "87383273232897",
-                "password": "3979s3239",
-                "role": "SHOPPER"
-            }),
-            headers: myHeaders,
-            credentials: 'include'
-        })
-        await response.text()
+    let response = await fetch('https://go-jwt-auth-production.up.railway.app/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            "fullName": "Henry Lotsu",
+            "email": "gaga12bobo @gmail.com",
+            "phone": "87383273232897",
+            "password": "3979s3239",
+            "role": "SHOPPER"
+        }),
+        headers: myHeaders,
+        credentials: 'include'
+    })
+    await response.text()
 
-        if (response.status === 409) {
-           console.error("The email already exists in the db bro!")
-        }
-        if (response.status === 201) {
-            console.log("Looks like you figured it out")
-        }
-        console.log(response.status)
+    if (response.status === 409) {
+        console.error("The email already exists in the db bro!")
+    }
+    if (response.status === 201) {
+        console.log("Looks like you figured it out")
+    }
+    console.log(response.status)
 
 }
 const SignUpAuth = () => {
@@ -35,14 +35,17 @@ const SignUpAuth = () => {
             <div className={form_control}>
                 <div className={input}>
                     <label className={label} htmlFor="name">Name</label>
-                    <motion.input className={text} type="text" name="name" id="name" placeholder={"Enter your first name"} initial={{opacity: 0,y: "40px"}} animate={{opacity: 1,y: 0}}/>
+                    <motion.input className={text} type="text" name="name" id="name"
+                                  placeholder={"Enter your first name"} initial={{opacity: 0, y: "40px"}}
+                                  animate={{opacity: 1, y: 0}}/>
                 </div>
             </div>
 
             <div className={form_control}>
                 <div className={input}>
                     <label className={label} htmlFor="email">Email</label>
-                    <motion.input className={text} type="text" name="email" id="email" placeholder={"Enter your email"} initial={{opacity: 0,y: "40px"}} animate={{opacity: 1,y: 0}}/>
+                    <motion.input className={text} type="text" name="email" id="email" placeholder={"Enter your email"}
+                                  initial={{opacity: 0, y: "40px"}} animate={{opacity: 1, y: 0}}/>
                 </div>
             </div>
 
@@ -50,20 +53,22 @@ const SignUpAuth = () => {
             <div className={form_control}>
                 <div className={input}>
                     <label className={label} htmlFor="password">Password</label>
-                    <motion.input className={text} type="password" name="password" id="password" placeholder={"**** (8 characters minimum) **** "} initial={{opacity: 0,y: "40px"}} animate={{opacity: 1,y: 0}}/>
+                    <motion.input className={text} type="password" name="password" id="password"
+                                  placeholder={"**** (8 characters minimum) **** "} initial={{opacity: 0, y: "40px"}}
+                                  animate={{opacity: 1, y: 0}}/>
                 </div>
             </div>
 
             <div className={form_control}>
-                <button className={auth} onClick={async (e)=> {
+                <button className={auth} onClick={async (e) => {
                     e.preventDefault()
                     await attemptLogin()
 
-                }} type={"submit"}>SignUp</button>
+                }} type={"submit"}>SignUp
+                </button>
             </div>
         </div>
     </form>
-
 }
 
 export default SignUpAuth
